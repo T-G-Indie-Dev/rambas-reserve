52f3c13d9aa8918e3e7e1520cec281d2edab0cfa
thalhamuk <thalhamuk@gmail.com>
2024-08-20 16:17:24 +0530

    Fix path not resolving issue in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
 	base: "/", 
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src')
+          "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
 		}
   	}
 })
