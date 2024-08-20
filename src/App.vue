5a49ddf40070ed71c9df4c264baa3db51a91aabf
thalhamuk <thalhamuk@gmail.com>
2024-08-20 14:58:53 +0530

    Initial project setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <p>Not a fan of hello world bullshit so here it is,</p>
+  <h1 class="text-3xl font-bold">Vue + Vim + Tailwind</h1>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
